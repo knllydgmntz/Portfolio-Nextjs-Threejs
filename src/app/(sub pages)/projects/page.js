043981f3c645031306d1d4f080f3d93995e56186
knllydgmntz <knllydgmntz@gmail.com>
2024-07-05 19:02:23 +0800

    adding projects and staff threejs

---
+++ src/app/(sub pages)/projects/page.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import bg from "../../../../public/background/projects-background.png";
+import ProjectList from "@/components/projects";
+import { projectsData } from "../../data";
+import Staff from "@/components/models/Staff";
+import RenderModel from "@/components/RenderModel";
+
+export default function Home() {
+  return (
+    <>
+      <Image
+        src={bg}
+        alt="background-image"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
+      />
+      <ProjectList projects={projectsData} />
+
+      <div className="flex items-center justify-center fixed top-20 -left-24 h-screen">
+        <RenderModel>
+          <Staff />
+        </RenderModel>
+      </div>
+    </>
+  );
+}
